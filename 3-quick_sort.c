61bd019a06248732a3a8d27fc53b8310be9de3f3
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-10-02 17:18:17 +0300

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -82,7 +82,7 @@ void lomuto_sort(int *array, size_t size, int left, int right)
  * order using the quicksort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
- * 
+ *
  * Description: Uses the Lomuto partition scheme. Prints
  * the array after each swap of two elements.
  */
@@ -90,6 +90,6 @@ void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
 		return;
-	
+
 	lomuto_sort(array, size, 0, size - 1);
 }
